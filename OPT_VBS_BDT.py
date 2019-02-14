8bf2f311c91c1baae56dd1c7104f922bb84faa7f
Benjamin Freund <benjamin.freund@cern.ch>
2019-02-14 17:06:08 -0500

    Adding BDT training.

---
+++ OPT_VBS_BDT.py
@@ -0,0 +1,152 @@
+from keras.utils.np_utils import to_categorical
+import sklearn
+from sklearn.externals import joblib
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.tree import DecisionTreeClassifier
+import numpy as np
+import pandas as pd
+from root_numpy import root2array, tree2array, array2root
+import ROOT
+import matplotlib.pyplot as plt
+from common_function import dataset, AMS, read_data, prepare_data
+
+def BDTModel(max_depth, learning_rate, n_estimators, algorithm):
+    BDT = AdaBoostClassifier(DecisionTreeClassifier(max_depth=max_depth),
+                         learning_rate=learning_rate,
+                         n_estimators=n_estimators,
+                         algorithm=algorithm)
+    return BDT
+
+#Directory where the ntuples are located
+filedir = '/lcg/storage15/atlas/freund/ntuples_Miaoran/'
+print('Read files from directory {0}'.format(filedir))
+#Assumed luminosity
+lumi=150.
+
+ 
+data_set=prepare_data(filedir, lumi)
+
+max_depth = 3
+learning_rate = 1
+n_estimators = 400
+algorithm = "SAMME"
+
+
+shape_train=data_set.X_train.shape
+shape_valid=data_set.X_valid.shape
+#shape_test=data_set.X_test.shape
+
+num_train=shape_train[0]
+num_valid=shape_valid[0]
+#num_test=shape_test[0]
+num_tot=num_train+num_valid#+num_test
+
+
+print("The number of training events {0} validation events {1} and total events {2}".format(num_train,num_valid,num_tot))
+
+model=BDTModel(max_depth, learning_rate, n_estimators, algorithm)
+
+print("Fit Model")
+
+model.fit(data_set.X_train, data_set.y_train[:,0].ravel())
+
+
+print("Save Model")
+
+filenameBDT = './modelBDT_train.pkl'
+_ = joblib.dump(model, filenameBDT, compress=9)
+
+# Plot the two-class decision scores
+plot_colors = "br"
+plot_step = 0.025
+twoclass_output = model.decision_function(data_set.X_train)
+plot_range = (twoclass_output.min(), twoclass_output.max())
+plt.subplot(111)
+class_names = "SB"
+
+print('Save decision plot')
+
+for i, n, c in zip(range(2), class_names, plot_colors):
+    plt.hist(twoclass_output[data_set.y_train[:,0] == i],
+             bins=20,
+             range=plot_range,
+             facecolor=c,
+             label='Class %s' % n,
+             alpha=.5,
+             #edgecolor='k',
+             log=True)
+x1, x2, y1, y2 = plt.axis()
+
+# Make a colorful backdrop to show the clasification regions in red and blue
+plt.axvspan(0, twoclass_output.max(), color='blue',alpha=0.08)
+plt.axvspan(twoclass_output.min(),0, color='red',alpha=0.08)
+
+plt.axis((x1, x2, y1, y2 * 1.2))
+plt.legend(loc='upper right')
+plt.ylabel('Samples')
+plt.xlabel('Score')
+plt.title('Decision Scores')
+
+plt.tight_layout()
+plt.subplots_adjust(wspace=0.35)
+plt.savefig('Decision_score_BDT.png')
+
+lower=-0.2
+upper=0.1
+stepsize=0.01
+
+AMS_train=np.zeros((int((upper-lower)/stepsize),2))
+AMS_valid=np.zeros((int((upper-lower)/stepsize),2))
+
+index2=0
+
+prob_predict_train_BDT = model.decision_function(data_set.X_train)
+prob_predict_valid_BDT = model.decision_function(data_set.X_valid)
+
+mass=300
+
+for loop2 in range(0,int((upper-lower)/stepsize)):
+
+    print "Cutting value {}".format(loop2*stepsize+lower)
+    
+    Yhat_train_BDT = prob_predict_train_BDT[:] > loop2*stepsize+lower
+    Yhat_valid_BDT = prob_predict_valid_BDT[:] > loop2*stepsize+lower
+    
+    s_train_BDT=b_train_BDT=0
+    s_valid_BDT=b_valid_BDT=0
+
+    for index in range(len(Yhat_train_BDT)):
+        if (Yhat_train_BDT[index]==1.0 and data_set.y_train[index,1]==1 and data_set.mass_train.iloc[index,0]>mass-mass*0.08*1.5 and data_set.mass_train.iloc[index,0]<mass+mass*0.08*1.5):
+            s_train_BDT +=  abs(data_set.W_train.iat[index,0]*(num_tot/num_train))
+        elif (Yhat_train_BDT[index]==1.0 and data_set.y_train[index,1]==0 and data_set.mass_train.iloc[index,0]>mass-mass*0.08*1.5 and data_set.mass_train.iloc[index,0]<mass+mass*0.08*1.5):
+            b_train_BDT +=  abs(data_set.W_train.iat[index,0]*(num_tot/num_train))
+
+    for index in range(len(Yhat_valid_BDT)):
+        if (Yhat_valid_BDT[index]==1.0 and data_set.y_valid[index,1]==1 and data_set.mass_valid.iloc[index,0]>mass-mass*0.08*1.5 and data_set.mass_valid.iloc[index,0]<mass+mass*0.08*1.5):
+            s_valid_BDT +=  abs(data_set.W_valid.iat[index,0]*(num_tot/num_valid))
+        elif (Yhat_valid_BDT[index]==1.0 and data_set.y_valid[index,1]==0 and data_set.mass_valid.iloc[index,0]>mass-mass*0.08*1.5 and data_set.mass_valid.iloc[index,0]<mass+mass*0.08*1.5):
+            b_valid_BDT +=  abs(data_set.W_valid.iat[index,0]*(num_tot/num_valid))
+
+    print "S and B NN training"
+    print s_train_BDT
+    print b_train_BDT
+    print "S and B NN validation"
+    print s_valid_BDT
+    print b_valid_BDT
+
+    print 'Calculating AMS score for BDTs with a probability cutoff pcut=',loop2*stepsize+lower
+    print '   - AMS based on 90% training   sample:',AMS(s_train_BDT,b_train_BDT)
+    print '   - AMS based on 10% validation sample:',AMS(s_valid_BDT,b_valid_BDT)
+    
+    AMS_train[index2,0]=loop2*stepsize+lower
+    AMS_train[index2,1]=AMS(s_train_BDT,b_train_BDT)
+    AMS_valid[index2,0]=loop2*stepsize+lower
+    AMS_valid[index2,1]=AMS(s_valid_BDT,b_valid_BDT)
+    index2=index2+1
+
+plt.plot(AMS_train[:,0],AMS_train[:,1], label='train')
+plt.plot(AMS_valid[:,0],AMS_valid[:,1], label='valid')
+plt.legend()
+plt.title('Significance as a function of the probability output')
+plt.savefig('significance_BDT.png')
+
---
+++ OPT_VBS_NN.py
@@ -0,0 +1,164 @@
+import keras
+from keras.utils.np_utils import to_categorical
+from keras.models import Sequential, load_model
+from keras.layers import Dense, Activation, Dropout
+from keras import backend as K
+from keras.optimizers import SGD
+from keras import optimizers
+import sklearn
+import numpy as np
+import pandas as pd
+import math
+from root_numpy import root2array, tree2array, array2root
+import ROOT
+from common_function import dataset, AMS, read_data, prepare_data, drawfigure
+
+def KerasModel(input_dim,numlayer,numn):
+    model = Sequential()
+    model.add(Dense(numn, input_dim=int(input_dim)))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.05))
+    for i in range(numlayer-1):
+        model.add(Dense(numn))
+        model.add(Activation('relu'))
+        model.add(Dropout(0.05))
+    model.add(Dense(2))
+    model.add(Activation('sigmoid'))
+
+    return model
+
+
+
+#Directory where the ntuples are located
+filedir = '/lcg/storage15/atlas/freund/ntuples_Miaoran/'
+#Assumed luminosity
+lumi=150.
+
+ 
+data_set=prepare_data(filedir, lumi)
+
+
+numlayer=3
+numn=200
+
+shape_train=data_set.X_train.shape
+shape_valid=data_set.X_valid.shape
+#shape_test=data_set.X_test.shape
+
+num_train=shape_train[0]
+num_valid=shape_valid[0]
+#num_test=shape_test[0]
+num_tot=num_train+num_valid#+num_test
+
+print(num_train,num_valid,num_tot)
+
+model=KerasModel(shape_train[1],numlayer,numn)
+sgd = optimizers.SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=False)
+ada= optimizers.Adadelta(lr=1, rho=0.95, epsilon=None, decay=0.01)
+model.compile(loss='binary_crossentropy', optimizer=sgd, metrics=['accuracy'])
+model.summary()
+model.save("modelNN_initial.h5")
+
+# Save the model architecture
+with open('model_architecture.json', 'w') as f:
+    f.write(model.to_json())
+
+epochs=80
+
+checkpoint=keras.callbacks.ModelCheckpoint(filepath='output_NN.h5', monitor='val_acc', verbose=1, save_best_only=True)
+logs = model.fit(data_set.X_train, data_set.y_train, epochs=epochs,
+                 validation_data=(data_set.X_valid, data_set.y_valid),batch_size=128, callbacks=[checkpoint], verbose =1)
+
+
+plt.plot(logs.history['acc'], label='train')
+plt.plot(logs.history['val_acc'], label='valid')
+plt.legend()
+plt.title('Pourcentage of correct classification')
+plt.savefig('class.png')
+plt.clf()
+plt.plot(logs.history['loss'], label='train')
+plt.plot(logs.history['val_loss'], label='valid')
+plt.legend()
+plt.title('Training loss')
+plt.savefig('loss.png')
+plt.clf() 
+
+#Calculate Significance
+#Load saved weights which gave best result on training
+model.load_weights('output_NN.h5')
+# Compile model (required to make predictions)
+model.compile(loss='binary_crossentropy', optimizer=sgd, metrics=['accuracy'])
+
+model.save('NN_model_complete.h5')
+
+prob_predict_train_NN = model.predict(data_set.X_train, verbose=False)
+prob_predict_valid_NN = model.predict(data_set.X_valid, verbose=False)
+#prob_predict_test_NN = model.predict(data_set.X_test, verbose=False)
+
+#lower=470
+lower=470
+upper=490
+massindex=0
+mass=300
+
+drawfigure(model,prob_predict_train_NN,data_set,data_set.X_valid)
+
+
+AMS_train=np.zeros((upper-lower,2))
+AMS_valid=np.zeros((upper-lower,2))
+index2=0
+#print(prob_predict_train_NN.shape)
+#for index in range(200):
+#    print "Label {}".format(data_set.y_train[index,1])
+#    print "Proba {}".format(prob_predict_train_NN[index,:])
+
+
+for loop2 in range(lower,upper):
+
+    print "With upper percentile {}".format(loop2/10.)
+    pcutNN = np.percentile(prob_predict_train_NN,loop2/10.)
+
+    print("Cut Value {}".format(pcutNN))
+    Yhat_train_NN = prob_predict_train_NN[:,1] > pcutNN
+    Yhat_valid_NN = prob_predict_valid_NN[:,1] > pcutNN
+    
+    
+
+    s_train_NN=b_train_NN=0
+    s_valid_NN=b_valid_NN=0
+
+    for index in range(len(Yhat_train_NN)):
+        if (Yhat_train_NN[index]==1.0 and data_set.y_train[index,1]==1 and data_set.mass_train.iloc[index,0]>mass-mass*0.08*1.5 and data_set.mass_train.iloc[index,0]<mass+mass*0.08*1.5 and data_set.mass_label_train.iloc[index,0]==massindex):
+            s_train_NN +=  abs(data_set.W_train.iat[index,0]*(num_tot/num_train))
+        elif (Yhat_train_NN[index]==1.0 and data_set.y_train[index,1]==0 and data_set.mass_train.iloc[index,0]>mass-mass*0.08*1.5 and data_set.mass_train.iloc[index,0]<mass+mass*0.08*1.5):
+            b_train_NN +=  abs(data_set.W_train.iat[index,0]*(num_tot/num_train))
+
+    for index in range(len(Yhat_valid_NN)):
+        if (Yhat_valid_NN[index]==1.0 and data_set.y_valid[index,1]==1 and data_set.mass_valid.iloc[index,0]>mass-mass*0.08*1.5 and data_set.mass_valid.iloc[index,0]<mass+mass*0.08*1.5 and data_set.mass_label_valid.iloc[index,0]==massindex):
+            s_valid_NN +=  abs(data_set.W_valid.iat[index,0]*(num_tot/num_valid))
+        elif (Yhat_valid_NN[index]==1.0 and data_set.y_valid[index,1]==0 and data_set.mass_valid.iloc[index,0]>mass-mass*0.08*1.5 and data_set.mass_valid.iloc[index,0]<mass+mass*0.08*1.5):
+            b_valid_NN +=  abs(data_set.W_valid.iat[index,0]*(num_tot/num_valid))
+
+    print "S and B NN training"
+    print s_train_NN
+    print b_train_NN
+    print "S and B NN validation"
+    print s_valid_NN
+    print b_valid_NN
+
+    print 'Calculating AMS score for NNs with a probability cutoff pcut=',pcutNN
+    print '   - AMS based on 90% training   sample:',AMS(s_train_NN,b_train_NN)
+    print '   - AMS based on 10% validation sample:',AMS(s_valid_NN,b_valid_NN)
+    
+    AMS_train[index2,0]=loop2/10.
+    AMS_train[index2,1]=AMS(s_train_NN,b_train_NN)
+    AMS_valid[index2,0]=loop2/10.
+    AMS_valid[index2,1]=AMS(s_valid_NN,b_valid_NN)
+    index2=index2+1
+
+plt.plot(AMS_train[:,0],AMS_train[:,1], label='train')
+plt.plot(AMS_valid[:,0],AMS_valid[:,1], label='valid')
+plt.legend()
+plt.title('Significance as a function of the probability output')
+plt.savefig('significance_NN.png')
+
